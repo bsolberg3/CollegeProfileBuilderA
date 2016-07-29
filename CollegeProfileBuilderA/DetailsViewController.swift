c42dc459ea1844a167b3c38761bfaed5ea2b4c9a
Bobby Solberg <bobbysolberg@bobbys-macbook-pro.local>
2016-07-29 11:59:27 -0500

    Added colleges

--- CollegeProfileBuilderA/DetailsViewController.swift
+++ CollegeProfileBuilderA/DetailsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailsViewController.swift
 //  CollegeProfileBuilderA
 //
 //  Created by Bobby Solberg on 7/27/16.
@@ -8,21 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
-    @IBOutlet weak var myTableView: UITableView!
-    @IBOutlet weak var editButton: UIBarButtonItem!
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        editButton.tag = 0
         colleges.append(College(name: "University of Miami", state: "Florida", enrollment: 9979, image: UIImage(named: "UofM")!))
         colleges.append(College(name: "University of Arizona", state: "Arizona", enrollment: 28210, image: UIImage(named: "UofA")!))
         colleges.append(College(name: "University of Texas-Austin", state: "Texas", enrollment: 39523, image: UIImage(named: "UTAustin")!))
-
     }
-
-
 }
-
